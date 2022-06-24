9254674bbf7d05cde97a4eed6f0a4614b5af4a58
Yuchen Zhang <gary.zhang@lifebyte.io>
2022-06-25 01:33:56 +1000

    update: user resource

--- src/app/Http/Resources/UserResource.php
+++ src/app/Http/Resources/UserResource.php
@@ -10,6 +10,18 @@ class UserResource extends JsonResource
 {
     public function toArray($request): array|JsonSerializable|Arrayable
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'department' => $this->department,
+            'job_title' => $this->job_title,
+            'location_office' => $this->location_office,
+            'location_position' => $this->location_position,
+            'state' => $this->state,
+            'is_admin' => (bool)$this->is_admin,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
     }
 }
