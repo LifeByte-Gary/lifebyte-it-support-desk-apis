21f5038d47959d8b1b028c37757ff92009662c20
Gary Zhang <gary.zhang@lifebyte.io>
2022-06-29 21:13:08 +1000

    update: location seeder

--- src/database/seeders/LocationSeeder.php
+++ src/database/seeders/LocationSeeder.php
@@ -12,24 +12,74 @@ class LocationSeeder extends Seeder
     {
         $offices = [
             [
-                'name' => '1011',
-                'company' => 'TMGM',
-                'country' => 'AU'
-            ],
-            [
                 'name' => '55C',
                 'company' => 'LifeByte',
-                'country' => 'AU'
+                'country' => 'Australia'
             ],
             [
-                'name' => '2800',
+                'name' => '2801',
                 'company' => 'TMGM',
-                'country' => 'AU'
+                'country' => 'Australia'
+            ],
+            [
+                'name' => '3003',
+                'company' => 'TMGM',
+                'country' => 'Australia'
+            ],
+            [
+                'name' => 'China Office',
+                'company' => 'TMGM',
+                'country' => 'China'
+            ],
+            [
+                'name' => 'India Office',
+                'company' => 'TMGM',
+                'country' => 'India'
+            ],
+            [
+                'name' => '7003',
+                'company' => 'TMGM',
+                'country' => 'Thailand'
+            ],
+            [
+                'name' => 'RV Office',
+                'company' => 'TMGM',
+                'country' => 'Australia'
+            ],
+            [
+                'name' => 'Cyprus Office',
+                'company' => 'TMGM',
+                'country' => 'Europe'
+            ],
+            [
+                'name' => 'France Office',
+                'company' => 'TMGM',
+                'country' => 'Europe'
+            ],
+            [
+                'name' => 'Germany Office',
+                'company' => 'TMGM',
+                'country' => 'Europe'
+            ],
+            [
+                'name' => 'Italy Office',
+                'company' => 'TMGM',
+                'country' => 'Europe'
+            ],
+            [
+                'name' => 'UK Office',
+                'company' => 'TMGM',
+                'country' => 'Europe'
+            ],
+            [
+                'name' => '6005',
+                'company' => 'TMGM',
+                'country' => 'New Zealand'
             ],
             [
                 'name' => '-',
                 'company' => 'TMGM',
-                'country' => 'AU'
+                'country' => 'Australia'
             ],
         ];
 
