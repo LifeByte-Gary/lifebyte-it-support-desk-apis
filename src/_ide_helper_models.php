bcb608bf44a6158c58b9bdb8f0797e79d796ba7e
Gary Zhang <gary.zhang@lifebyte.io>
2022-07-14 11:22:46 +1000

    update: hardware update request validation rules

--- src/_ide_helper_models.php
+++ src/_ide_helper_models.php
@@ -14,49 +14,11 @@ namespace App\Models{
 /**
  * App\Models\Hardware
  *
- * @property int $id
- * @property int $user_id
- * @property string $name
- * @property string|null $description
- * @property string $type
- * @property string $brand
- * @property string $model
- * @property string $serial_number
- * @property string $tag
- * @property string|null $spec_os
- * @property string|null $spec_cpu
- * @property float|null $spec_memory
- * @property int|null $spec_storage
- * @property float|null $spec_screen_size
- * @property string|null $spec_others
- * @property array|null $bundle_with
- * @property string|null $note
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
  * @property-read \App\Models\User|null $user
  * @method static \Database\Factories\HardwareFactory factory(...$parameters)
  * @method static \Illuminate\Database\Eloquent\Builder|Hardware newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Hardware newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Hardware query()
- * @method static \Illuminate\Database\Eloquent\Builder|Hardware whereBrand($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Hardware whereBundleWith($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Hardware whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Hardware whereDescription($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Hardware whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Hardware whereModel($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Hardware whereName($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Hardware whereNote($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Hardware whereSerialNumber($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Hardware whereSpecCpu($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Hardware whereSpecMemory($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Hardware whereSpecOs($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Hardware whereSpecOthers($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Hardware whereSpecScreenSize($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Hardware whereSpecStorage($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Hardware whereTag($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Hardware whereType($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Hardware whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Hardware whereUserId($value)
  */
 	class Hardware extends \Eloquent {}
 }
@@ -65,17 +27,11 @@ namespace App\Models{
 /**
  * App\Models\Location
  *
- * @property int $id
- * @property string $name
- * @property string $country
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\User[] $users
  * @property-read int|null $users_count
  * @method static \Illuminate\Database\Eloquent\Builder|Location newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Location newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|Location query()
- * @method static \Illuminate\Database\Eloquent\Builder|Location whereCountry($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Location whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Location whereName($value)
  */
 	class Location extends \Eloquent {}
 }
@@ -87,16 +43,8 @@ namespace App\Models{
  * @property int $id
  * @property string $name
  * @property string $email
- * @property string $password
- * @property string $company
- * @property string|null $department
- * @property string|null $job_title
- * @property int $location_id
- * @property string|null $desk
- * @property int $state
- * @property string $type
- * @property int $permission_level
  * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
  * @property string|null $remember_token
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
@@ -111,21 +59,13 @@ namespace App\Models{
  * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|User query()
- * @method static \Illuminate\Database\Eloquent\Builder|User whereCompany($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereDepartment($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereDesk($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereJobTitle($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereLocationId($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereName($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User wherePermissionLevel($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereState($value)
- * @method static \Illuminate\Database\Eloquent\Builder|User whereType($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
  */
 	class User extends \Eloquent {}
